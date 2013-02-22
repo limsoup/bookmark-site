698b8a10cc8ee0e99db4bc5463e6452d24f1f8ae
Sakif Imtiaz <sakif@umich.edu>
2013-02-22 18:44:29 -0500

    did most of the bookmark ajax features, handling the layouts

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -5,92 +5,153 @@
 
 (function( $ ) {
   $.fn.bookmarkSetup = function (){
-		this.find('a.edit_bookmark_button').on('click', function(event){
-			event.preventDefault();
-			$(this).parents('.show_bookmark').first().hide().next().show();
-		});
-		this.find('a.show_bookmark_button').on('click', function(event){
-			event.preventDefault();
-			$(this).parents('.edit_bookmark').first().hide().prev().show();
-		});
-		this.find('a.remove_bookmark').on('ajax:success', function(){
-			$(this).parents('.bookmark').first().remove();
-		});
-		this.find('form').on('ajax:success', function(event, data, status, xhr){
-			if($(this).is(event.target))
-			{
-				$(this).parents('.bookmark').first().html(data).bookmarkSetup();
-			}
-		});
-		//when edit fields input changes
-		this.find('.edit_bookmark_fields input').on('change', function (){
-			$(this).parent().siblings().first().hide().next().show().next().show().next('.move_bookmark').hide(); 
-			//hides the 'collapse', shows the 'reveal' and 'save', and hides the 'move'
-		});
-		//revert (show) or save (update)
-		this.find('.reload_edit_bookmark_button').on('ajax:success', function(event, data, status, xhr){
-			$(this).parents('.bookmark').first().html(data).bookmarkSetup();
-		});
-		this.find('.update_bookmark_button').on('click', function(){
-			$(this).parents('form').submit();
-		}).on('ajax:success', function(event, data, status, xhr){
-			$(this).parents('.bookmark').first().html(data).bookmarkSetup();
-		});
-		//move bookmark
-		this.find('.move_bookmark_submit').on('ajax:before', function(event){
-			var sendBack = {};
-			sendBack.moveType = $(this).siblings('[name=user_bookmark_move]:checked').first().val();
-			sendBack.destination = $(this).siblings('[name=user_bookmark_move_paste]').first().val();
-			$(this).data('params',jQuery.param(sendBack));
-		}).on('ajax:success', function(event, data, status, xhr){
-			// console.log('lol');
-			// console.log(data);
-			if(data["delete"] == true)
-			{
-				// console.log('data');
-				// console.log(data);
-				$(this).parents('.bookmark').first().remove();
-			}
-			return false;
-		});
+  	this.on('click', function(event){
+  		if($(this).find(ubNameSel).val() != detail.find(ubNameSel).val() ){
+  			if(detail.children().length > 0) {
+  				detail.children().replaceWith($(this).find('.detail_bookmark').clone().show());
+  			} else{
+					detail.append($(this).find('.detail_bookmark').clone().show());
+  				detail.parent().addClass('detail_container_color');
+  			}
+  		}
+  	});
 	};
 })( jQuery );
 
 
 $(function() {
-	//--applies to edit playlist pages--
-	//hide HTML-Request New Bookmark Links
+	//--applies to edit bookmark pages--
+
 	$('#add_bookmark_html').hide();
-	//unhide Javascript-Request New Bookmark Links
 	$('#add_bookmark_js').show();
-	//replaces bookmark div after update is successful on server's side
+
+	//set event handlers for bookmarks
+
 	$('.bookmark').bookmarkSetup();
+	ubNameSel='input[name="user_bookmark[bookmark_url_attributes][id]"]';
+	detail = $('#detail_bookmark_view');
+	function findBookmarkInList(){
+		return $('#bookmark_list_view').find(ubNameSel+'[value='+detail.find(ubNameSel).val()+']').parents('.bookmark');
+	}
+
+	$('#filter_uncategorized, #filter_bookmarks').on('keyup', function(event){
+		filterField = this;
+		if($(this).val() == ''){
+			$('.bookmark').show();
+		}else{
+			$('.bookmark').each( function(index){
+				if ($(this).find('a').first().text().toLowerCase().search($(filterField).val().toLowerCase()) == -1){
+					$(this).hide();
+				}else{
+					$(this).show();
+				}
+			});
+		}
+		if(detail.children().length != 0){
+			if($(findBookmarkInList()).css('display')=='none') {
+				detail.children().remove();
+				detail.parent().removeClass('detail_container_color');
+			}
+		}
+	});
+
+	//detail actions
+
+	detail.on('click', 'a.collapse_link', function(){
+		detail.children().remove();
+		detail.parent().removeClass('detail_container_color');
+	});
+
+	detail.on('click', 'a.edit_link', function(){
+		if($(this).hasClass('active')){
+			detail.find('.edit_bookmark_fields').hide();
+		} else{
+			detail.find('.edit_bookmark_fields').show();
+		}
+		if(detail.find('a.move_link').hasClass('active')){
+			$(detail.find('a.move_link')).trigger('click');
+		}
+	});
+
+	detail.on('click', 'a.move_link', function(){
+		if($(this).hasClass('active')){
+			detail.find('.move_bookmark').hide();
+		} else{
+			detail.find('.move_bookmark').show();
+		}
+	});
+
+	detail.on('keyup','.edit_bookmark_fields input', function (){
+		//hides the 'collapse', shows the 'reload' and 'save', and hides the 'move'
+		detail.find('a.collapse_link').addClass('disabled');
+		detail.find('a.revert_link').show();
+		detail.find('a.save_link').show();
+		detail.find('a.move_link').addClass('disabled');
+		if(detail.find('a.move_link').hasClass('active')){
+			$(detail.find('a.move_link')).trigger('click');
+		}
+		detail.find('a.edit_link').addClass('disabled');
+	});
+
+	detail.on('click', 'a.revert_link', function(){
+		detail.find('.edit_bookmark_fields').replaceWith($(findBookmarkInList()).find('.edit_bookmark_fields').clone().show());
+		detail.find('a.collapse_link').removeClass('disabled');
+		detail.find('a.move_link').removeClass('disabled');
+		detail.find('a.edit_link').removeClass('disabled');
+		detail.find('a.revert_link').hide();
+		detail.find('a.save_link').hide();
+	});
+
+	detail.on('click', 'a.save_link', function(){
+		detail.find('form').submit();
+	}).on('ajax:success', 'form', function(event, data, status, xhr){
+		source = $(findBookmarkInList());
+		$(findBookmarkInList()).html(data).bookmarkSetup();
+		//copy the *contents* of the pane classes except the embed stuff
+		detail.find('.show_bookmark_detail').html(source.find('.show_bookmark_detail').clone().children());
+		detail.find('.edit_bookmark_fields').html(source.find('.edit_bookmark_fields').clone().children());
+		detail.find('.move_bookmark').html(source.find('.move_bookmark').clone().children());
+		//set the links right
+		detail.find('a.collapse_link').removeClass('disabled');
+		detail.find('a.move_link').removeClass('disabled');
+		detail.find('a.edit_link').removeClass('disabled');
+		detail.find('a.revert_link').hide();
+		detail.find('a.save_link').hide();
+	});
+
+	detail.on('ajax:success','a.remove_link', function(){
+		$(findBookmarkInList()).remove();
+		detail.children().remove();
+		detail.parent().removeClass('detail_container_color');
+	});
+	
+	detail.on('ajax:before', '.move_bookmark_submit' function(event){
+		var sendBack = {};
+		sendBack.moveType = $(this).siblings('[name=user_bookmark_move]:checked').first().val();
+		sendBack.destination = $(this).siblings('[name=user_bookmark_move_paste]').first().val();
+		$(this).data('params',jQuery.param(sendBack));
+	}).on('ajax:success', function(event, data, status, xhr){
+		if(data["delete"] == true){
+			$(findBookmarkInList()).remove();
+			detail.children().remove();
+			detail.parent().removeClass('detail_container_color');
+		}
+		return false;
+	});
+
 	//--applies to pages where playlists can be edited--
+
 	$('.remove_playlist').on('ajax:success', function (event, data, status, xhr){
 		this.parents('.playlist_link').first().remove();
 	});
-	//--applies to filter
-	$('#filter_uncategorized').on('change', function(){
-			//keep visible
-			if($('#filter_uncategorized').val() == ''){
-				$('.bookmark').show();
-			}
-			else{
-				console.log('filtering');
-				$('.bookmark').filter( function(index){
-					if ($(this).find('a').first().text().toLowerCase().search($('#filter_uncategorized').val().toLowerCase()) == -1){
-						$(this).hide();
-					}
-				});
-			}
-		});
+	
 	//--applies to playlist#show which has linkDrop
 
 	$('#linkDrop').on('dragover dragenter', function (e) {
 	  if (e.preventDefault) e.preventDefault(); // required by FF + Safari
-	  e.originalEvent.dataTransfer.dropEffect = 'copy'; // tells the browser what drop effect is allowed here
-	  return false; // required by IE
-	}).on('drop', function (e){
+		  e.originalEvent.dataTransfer.dropEffect = 'copy'; // tells the browser what drop effect is allowed here
+		  return false; // required by IE
+		}).on('drop', function (e){
 		if (e.preventDefault) e.preventDefault();
 		if (e.originalEvent.dataTransfer.types) {
 			[].forEach.call(e.originalEvent.dataTransfer.types, function(type) {
