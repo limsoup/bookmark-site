d92887db74273417ac6815c8494b7e22f92747fb
Sakif Imtiaz <sakif@umich.edu>
2013-03-04 18:40:28 -0500

    playthru invisble hiding

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -13,12 +13,18 @@ $(function() {
 	// 	$('#modalHolder').find('.modal').modal('show');
 	// });
 
-	$('.signup-modal-link').on('ajax:success', function(event, data, status, xhr){
-		$('#modalHolder').html(data);
+	// $('.signup-modal-link').on('ajax:success', function(event, data, status, xhr){
+	// 	$('#modalHolder').html(data);
+	// 	$('#modalHolder').find('.modal').modal('show');
+	// });
+
+	$('#signup-modal-link').on('click', function(event){
+		event.preventDefault();
+		$('#signup-modal-holder').find('.modal').clone().appendTo('#modalHolder');
 		$('#modalHolder').find('.modal').modal('show');
 	});
 
-	$("#modalHolder").on('ajax:success', '#signup-modal form', function(event, data, status, xhr){
+	$("#modalHolder").on('ajax:success', '.signup-modal form', function(event, data, status, xhr){
 		//display success
 		//if it's added to this playlist, add bookmark
 		//if it's added to some other list, give choice between staying here or going to that list
@@ -38,10 +44,7 @@ $(function() {
 		}
 		//disable form
 		//redirect to form page
-	}).on('shown', '#signup-modal', function(){
-		$(document.body).delay(500).trigger('load');
-		console.log('lol');
-	}).on('close', '#signup-modal form .alert', function(){
+	}).on('close', '.signup-modal form .alert', function(){
 		$(this).after($(this).clone());
 		$(this).parent().find('div').each(function(index){
 			if(index > 1) {
@@ -49,12 +52,12 @@ $(function() {
 				console.log($(this).text());
 			}
 		});
-	}).on('closed', '#signup-modal .alert', function(){
+	}).on('closed', '.signup-modal .alert', function(){
 		//$('#modalHolder #signup-modal input[type=text]').val('');
 		//$('#modalHolder #signup-modal input[type=password]').val('');
-		$('#modalHolder #signup-modal .alert-container-success').hide();
-		$('#modalHolder #signup-modal .alert-container-error').hide();
-		$('#modalHolder #signup-modal .form-buttons').show();
+		$('#modalHolder .signup-modal .alert-container-success').hide();
+		$('#modalHolder .signup-modal .alert-container-error').hide();
+		$('#modalHolder .signup-modal .form-buttons').show();
 	});;
 
 });
