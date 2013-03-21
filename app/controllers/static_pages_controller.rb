ab03f78f767dd591e2cdce7f6509de0b81afb381
Sakif Imtiaz <sakif@umich.edu>
2013-03-21 18:53:45 -0400

    ub fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -20,12 +20,12 @@ class StaticPagesController < ApplicationController
 			if (@user.save)
 				session[:remember_token] = @user.remember_token	
 				ub1 = @user.default_list.user_bookmarks.build
-				ub1.name = "Two Really Hot Italian Soccer Fans Wrestle and Scissor it out."
+				ub1.bookmark_name = "Two Really Hot Italian Soccer Fans Wrestle and Scissor it out."
 				bu1 = BookmarkUrl.find(62)
 				ub1.bookmark_url = bu1;
 				ub1.save
 				ub2 = @user.default_list.user_bookmarks.build
-				ub2.name = "Wholesome-looking Mormon girl gets it."
+				ub2.bookmark_name = "Wholesome-looking Mormon girl gets it."
 				bu2 = BookmarkUrl.find(63)
 				ub2.bookmark_url= bu2
 				ub2.save
